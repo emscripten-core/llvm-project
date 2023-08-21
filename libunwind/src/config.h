1ec6f5ddf0919dcf10d242014582e986c0de9e10
Heejin Ahn <aheejin@gmail.com>
2023-08-21 16:19:29 -0700

    Update libunwind to llvm 16.0.6
    Drive-by update of libcxx version number to 160006 to make all library
    versions consistent at 16.0.6. 16.0.4->16.0.6 doesn't have any other
    changes in other libraries.

--- libunwind/src/config.h
+++ libunwind/src/config.h
@@ -98,6 +98,7 @@
                                              SYMBOL_NAME(name)))               \
   extern "C" _LIBUNWIND_EXPORT __typeof(name) aliasname;
 #endif
+#elif defined(__EMSCRIPTEN__) // XXX EMSCRIPTEN
 #else
 #error Unsupported target
 #endif
